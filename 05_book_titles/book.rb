59923252ce776f4ed45d4cd955ba939cd7961ec5
Bruke Mammo <bruke.mammo@gmail.com>
2016-07-25 15:25:50 -0400

    completed all 7 challenges

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,20 @@
 class Book
 # write your code here
+
+  def title=(str)
+    words = str.split(" ").map do |word|
+      if %w(the and over in of to a an).include?(word)
+        word
+      else
+        word.capitalize
+      end
+    end
+    words.first.capitalize!
+    @title = words.join(" ")
+  end
+
+  def title
+    @title
+  end
+
 end
--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,43 @@
 class Timer
   #write your code here
+  attr_accessor :seconds
+  def initialize
+    @seconds = 0
+  end
+
+  def time_string
+    seconds = @seconds
+    if seconds==0
+      "00:00:00"
+    else
+      if seconds>=3600
+        hours = seconds/3600
+        seconds = seconds - (3600*hours)
+      end
+      if seconds>=60
+        minutes = seconds/60
+        seconds = seconds - (60*minutes)
+      end
+      if hours.nil?
+        hours="00"
+      elsif hours<10
+        hours = "0"+hours.to_s
+      else
+        hours = hours.to_s
+      end
+      if minutes.nil?
+        minutes="00"
+      elsif minutes<10
+        minutes = "0"+minutes.to_s
+      else
+        minutes = minutes.to_s
+      end
+      if seconds<10
+        seconds = "0"+seconds.to_s
+      else
+        seconds = seconds.to_s
+      end
+      hours+":"+minutes+":"+seconds
+    end
+  end
 end
